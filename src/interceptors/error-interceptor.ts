5fc8d7fceb013168fb9bd80bb1d7e90a8aafe435
Kleverland Formiga <kleverland@gmail.com>
2020-04-11 10:30:57 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,70 +3,94 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService, public alertCtrl: AlertController){}
+    constructor(public storage: StorageService, public alertCtrl: AlertController) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         return next.handle(req)
-        .catch((error, caught) => {
+            .catch((error, caught) => {
 
-            let errorObj = error;
-            if (errorObj.error) {
-                errorObj = errorObj.error;
-            }
-            if (!errorObj.status) {
-                errorObj = JSON.parse(errorObj);
-            }
+                let errorObj = error;
+                if (errorObj.error) {
+                    errorObj = errorObj.error;
+                }
+                if (!errorObj.status) {
+                    errorObj = JSON.parse(errorObj);
+                }
 
-            console.log("Erro detectado pelo interceptor:");
-            console.log(errorObj);
+                console.log("Erro detectado pelo interceptor:");
+                console.log(errorObj);
 
-            switch(errorObj){
-                case 401:
-                    this.handle401();
-                    break;
+                switch (errorObj.status) {
+                    case 401:
+                        this.handle401();
+                        break;
 
-                case 403:
-                    this.handle403();
-                    break;
-                default:
-                    this.handleDefaultError(errorObj);
-            }
-
-
-            return Observable.throw(errorObj);
-        }) as any;
+                    case 403:
+                        this.handle403();
+                        break;
+                    case 422:
+                        this.handle422(errorObj);
+                        break;
+                    default:
+                        this.handleDefaultError(errorObj);
+                }
+                return Observable.throw(errorObj);
+            }) as any;
     }
 
-    handle403(){
+    handle403() {
         this.storage.setLocalUser(null);
     }
 
-    handle401(){
+    handle401() {
         let alert = this.alertCtrl.create({
             title: 'Erro 401: Falha de autenticação',
             message: 'Email ou senha incorretos',
             enableBackdropDismiss: false,
-            buttons:[{
-                text:'Ok'
+            buttons: [{
+                text: 'Ok'
             }]
         });
         alert.present();
     }
-    handleDefaultError(errorObj){
+
+    handle422(errorObj) {
         let alert = this.alertCtrl.create({
-            title: 'Erro '+ errorObj.status + ': ' + errorObj.error ,
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultError(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
             message: errorObj.message,
             enableBackdropDismiss: false,
-            buttons:[{
-                text:'Ok'
+            buttons: [{
+                text: 'Ok'
             }]
         });
         alert.present();
+    }
+    private listErrors(messages: FieldMessage[]): string {
+        let s: string = '';
+        for (var i = 0; i < messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
     }
 }
 
