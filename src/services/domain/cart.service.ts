71a2bf3affc54306b106a9894a463476ff614964
Kleverland Formiga <kleverland@gmail.com>
2020-04-12 11:07:12 -0300

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { StorageService } from '../storage.service';
 import { Cart } from '../../models/cart';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { Footer } from 'ionic-angular';
 
 @Injectable()
 export class CartService {
@@ -31,5 +32,47 @@ export class CartService {
         }
         this.storage.setCart(cart);
         return cart;
+    }
+
+    removeProduto(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items.splice(position,1);
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    increaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items[position].quantidade++;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    decreaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items[position].quantidade--;
+            if(cart.items[position].quantidade < 1 ){
+                cart = this.removeProduto(produto);
+            }
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    total() : number {
+        let cart = this.getCart();
+        let sum = 0;
+        for (let i = 0; i < cart.items.length; i++) {
+            sum += cart.items[i].produto.preco*cart.items[i].quantidade;            
+        }
+        return sum;
     }
 }
