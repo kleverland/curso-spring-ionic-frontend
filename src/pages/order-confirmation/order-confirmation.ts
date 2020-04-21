649c0af999d14a286fc20923d1310ff3ca56b8e2
Kleverland Formiga <kleverland@gmail.com>
2020-04-21 11:18:18 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -20,6 +20,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -56,12 +57,15 @@ export class OrderConfirmationPage {
   back(){
     this.navCtrl.setRoot('CartPage');
   }
+  home(){
+    this.navCtrl.setRoot('CategoriasPage')
+  }
 
   checkout(){
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if(error.status == 403){
@@ -70,4 +74,8 @@ export class OrderConfirmationPage {
       });
   }
 
+  private extractId(location : string) : string{
+    let position = location.lastIndexOf('/');
+    return location.substring(position+1, location.length);
+  }
 }
